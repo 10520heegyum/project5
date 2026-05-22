5fb56f13863643d33b75308d80819d05f86af32e
10520heegyum <dg2026520@danggok.hs.kr>
2026-05-22 12:10:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import random
+
+balls = []
+
+for i in range(5):
+
+    ball = sphere(
+        pos=vector(random.randint(-10,-5),random.randint(-5,5),0),
+        radius=0.7,
+        color=color.red)
 
