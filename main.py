649b81fb10cd26609b6071d6647a13a6d5a1027d
10520heegyum <dg2026520@danggok.hs.kr>
2026-05-26 12:07:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ for i in range(5):
     ball = sphere(pos=vector(random.randint(-10,-5),
             random.randint(-5,5),0),
         radius=0.7,
-        color=color.yellow)
+        color=color.yellow)]
+while True : 
+    k = keysdown()
+     if ' 'in k :
+       ball.pos.x = random.uniform(-10, 5)
+       ball.pos.y = random.uniform(-5, 5)
 
 
