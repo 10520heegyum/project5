fef8f7a43b20abfafa8221e692f6142f39c6128f
10520heegyum <dg2026520@danggok.hs.kr>
2026-05-26 11:30:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ for i in range(5):
 for i in range(5):
 
     ball = sphere(pos=vector(random.randint(-10,-5),
-            random.randint(-5,5),
-            0),
+            random.randint(-5,5),0),
         radius=0.7,
         color=color.yellow)
 
