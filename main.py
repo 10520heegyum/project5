b707250a2a88adf3bf73fc4c0232ccca0c40854e
10520heegyum <dg2026520@danggok.hs.kr>
2026-05-29 11:56:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from vpython import *
 import random
 
-balls = []
+scene.background = color.black
+scene.title = "Red vs Yellow - Day 1"
+
+red_balls = []
+yellow_balls = []
 
 for i in range(5):
+    red_balls.append(sphere(pos=vector(random.uniform(-5,-3), random.uniform(-3,3),0),
+                            radius=0.4, color=color.red))
+    yellow_balls.append(sphere(pos=vector(random.uniform(-5,-3), random.uniform(-3,3),0),
+                               radius=0.4, color=color.yellow))
 
-    ball = sphere(
-        pos=vector(random.randint(-10,-5),random.randint(-5,5),0),
-        radius=0.7,
-        color=color.red)
-for i in range(5):
+launcher = arrow(pos=vector(5,0,0), axis=vector(-2,0,0),
+                 shaftwidth=0.3, color=color.cyan)
 
-    ball = sphere(pos=vector(random.randint(-10,-5),
-            random.randint(-5,5),0),
-        radius=0.7,
-        color=color.yellow)]
-while True : 
-   rate(100)
-   k = keysdown()
-   if ' 'in k :
-       ball.pos.x = random.uniform(-10, 5)
-       ball.pos.y = random.uniform(-5, 5)
+launcher_dir = 1
 
+while True:
+    rate(60)
+    launcher.pos.y += 0.05 * launcher_dir
+    if abs(launcher.pos.y) > 3:
+        launcher_dir *= -1
